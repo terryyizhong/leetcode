fec231b6428e77da63a680258b04612b8aa2ad4f
terryyizhong <yi.zhong@rutgers.edu>
2017-05-14 23:24:16 -0400

    Create 110. Balanced Binary Tree.py

---
+++ search/110. Balanced Binary Tree.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def check(root):
+            if not root:
+                return 0
+            l = check(root.left)
+            r = check(root.right)
+            if abs(l-r) > 1 or l == -1 or r == -1:
+                return -1
+            else:
+                return max(l, r) + 1
+        return check(root) != -1
+
+#dfs version
+class Solution(object):
+    def isBalanced(self, root):
+        stack, node, last, depths = [], root, None, {}
+        while stack or node:
+            if node:
+                stack.append(node)
+                node = node.left
+            else:
+                node = stack[-1]
+                if not node.right or last == node.right:
+                    node = stack.pop()
+                    left, right  = depths.get(node.left, 0), depths.get(node.right, 0)
+                    if abs(left - right) > 1: return False
+                    depths[node] = 1 + max(left, right)
+                    last = node
+                    node = None
+                else:
+                    node = node.right
+        return True

aa2f20c19acc5cee4cbb80d1a049f2eb966efa59
terryyizhong <yi.zhong@rutgers.edu>
2017-09-24 18:35:12 -0400

    Create mid_133. Clone Graph.py

---
+++ search/mid_133. Clone Graph.py
@@ -0,0 +1,52 @@
+'''
+
+Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+
+
+OJ's undirected graph serialization:
+Nodes are labeled uniquely.
+
+We use # as a separator for each node, and , as a separator for node label and each neighbor of the node.
+As an example, consider the serialized graph {0,1,2#1,2#2,2}.
+
+The graph has a total of three nodes, and therefore contains three parts as separated by #.
+
+First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
+Second node is labeled as 1. Connect node 1 to node 2.
+Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
+Visually, the graph looks like the following:
+
+       1
+      / \
+     /   \
+    0 --- 2
+         / \
+         \_/
+'''
+# Definition for a undirected graph node
+# class UndirectedGraphNode:
+#     def __init__(self, x):
+#         self.label = x
+#         self.neighbors = []
+
+class Solution:
+    # @param node, a undirected graph node
+    # @return a undirected graph node
+    def cloneGraph(self, node):
+        if not node:
+            return None
+        queue = collections.deque([node])
+        node_new = UndirectedGraphNode(node.label)
+        node_dict = {node: node_new}
+        while queue:
+            curr = queue.popleft()
+            for neighbor in curr.neighbors:
+                if neighbor not in node_dict:
+                    new_neighbor = UndirectedGraphNode(neighbor.label)
+                    node_dict[neighbor] = new_neighbor
+                    node_dict[curr].neighbors.append(new_neighbor)
+                    queue.append(neighbor)
+                else:
+                    node_dict[curr].neighbors.append(node_dict[neighbor]) 
+            
+        return node_new

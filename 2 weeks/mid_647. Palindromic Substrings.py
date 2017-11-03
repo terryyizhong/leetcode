a177e9e4c5a69bc3ac6fe70656044a690dbb7d60
terryyizhong <yi.zhong@rutgers.edu>
2017-11-03 17:09:45 -0400

    Create mid_647. Palindromic Substrings.py

---
+++ 2 weeks/mid_647. Palindromic Substrings.py
@@ -0,0 +1,52 @@
+'''
+Given a string, your task is to count how many palindromic substrings in this string.
+
+The substrings with different start indexes or end indexes are counted as different substrings even they consist of same characters.
+
+Example 1:
+Input: "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+Input: "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+Note:
+The input string length won't exceed 1000.
+'''
+
+
+# easy but slow O(n2) solution
+class Solution(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res = 0
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                if s[i:j+1] == s[i:j+1][::-1]:
+                    res += 1
+        return res
+'''        
+We perform a "center expansion" among all possible centers of the palindrome.
+
+Let N = len(S). There are 2N-1 possible centers for the palindrome: we could have a center at S[0], between S[0] and S[1], at S[1], between S[1] and S[2], at S[2], etc.
+
+To iterate over each of the 2N-1 centers, we will move the left pointer every 2 times, and the right pointer every 2 times starting with the second (index 1). Hence, left = center / 2, right = center / 2 + center % 2.
+
+From here, finding every palindrome starting with that center is straightforward: while the ends are valid and have equal characters, record the answer and expand.
+'''
+# save a lot comparison, worst case still O(n2) but a lot more fast, each time only need compare 1 element and stop if not same
+def countSubstrings(self, S):
+    N = len(S)
+    ans = 0
+    for center in xrange(2*N - 1):
+        left = center / 2
+        right = left + center % 2
+        while left >= 0 and right < N and S[left] == S[right]:
+            ans += 1
+            left -= 1
+            right += 1
+    return ans        
